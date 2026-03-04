90bcb4b1824f8e701e87819a917ddf3e3635872d
ehiemerepraise91 <ehiemerepraise91@gmail.com>
2026-03-04 11:39:44 +0100

    apps completed

---
+++ app.py
@@ -0,0 +1,9 @@
+# print ("My name is praise")
+# print ( "praise is learning python")
+# print ( "praise wants to become a python")
+ 
+name = "praise"
+language = "Python"
+print("my name is", name)
+print(name,"is learning",language)      
+print(name, "wants to become a", language, "expert")
